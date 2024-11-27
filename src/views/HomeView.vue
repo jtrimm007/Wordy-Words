3de21cb4f8e062d19367742adc900ff4b66e9bfc
Joshua Trimm <jbthype@gmail.com>
2024-11-26 21:42:17 -0500

    asdf

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-      <div>
-    <p v-if="peerConnected">My peer ID is: {{ peerId }}</p>
-    <button v-if="peerConnected" @click="copyPeerId">Copy ID</button>
-  </div>
+    <!-- <div>
+      <h2>Connections</h2>
+      <ul>
+        <li v-for="conn in connections" :key="conn.peer">
+          <span>{{ conn.userDetails.username }}</span>
+
+        </li>
+      </ul>
+    </div> -->
+    <div>
+      <p v-if="peerConnected">My peer ID is: {{ peerId }}</p>
+      <button v-if="peerConnected" @click="copyPeerId">Copy ID</button>
+    </div>
     <input v-model="username" placeholder="Enter your username" />
     <button @click="initializePeer">Join Chat</button>
     <input v-model="connectionId" placeholder="Enter connection id" />
@@ -19,7 +28,7 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
 import { v4 as uuidv4 } from 'uuid'
 import Peer from 'peerjs'
 
@@ -58,22 +67,24 @@ const getIceServers = async () => {
     return []
   }
 }
-// Assuming you have a list of peer IDs
+
 const connectToPeer = () => {
   if (!peer) {
     console.error('Peer is not initialized')
     return
   }
   console.log('connecting to peer', connectionId.value)
-  const conn = peer.connect(connectionId.value)
+  const conn = peer.connect(connectionId.value, { metadata: { id: userId.value, username: username.value } })
 
+  // this is the initial connection from an incoming peer.
   conn.on('open', () => {
     console.log(`Connected to peer: ${connectionId.value}`)
-    connections.value.push(conn)
+    conn.metadata.connectingPeer = { id: userId.value, username: username.value }
 
-    // Handle incoming data
-    // conn.on('data', receiveMessage)
-    conn.send({ username: username.value, text: 'Hello from ' + username.value })
+    connections.value.push(conn)
+    console.log('connections.value');
+    console.log(connections.value);
+    conn.send({ msgId: uuidv4(), username: username.value, text: 'Hello from ' + username.value })
   })
   conn.on('data', (data) => {
     console.log('connect to peer data')
@@ -114,23 +125,28 @@ const initializePeer = async () => {
       ]
     }
   })
-
+  // peer.userDetails = { id: userId.value, username: username.value }
   peer.on('open', (id) => {
     peerId.value = id
     peerConnected.value = true
     console.log('My peer ID is:', id)
   })
 
+  // this is the connection coming from another peer.
   peer.on('connection', (conn) => {
+    conn.metadata.initPeer = { id: userId.value, username: username.value }
     connections.value.push(conn)
-
+    console.log('connections.value');
+    console.log(connections.value);
     conn.on('data', (data) => {
       chatHistory.value.push({ ...data, id: userId.value, date: Date.now() })
       const broadcastMessage = (message) => {
         connections.value.forEach((conn) => {
           if (conn.open) {
             console.log('broadcastMessage')
-            conn.send(message)
+            console.log(message, chatHistory.value);
+            conn.send(message, chatHistory.value)
+            // conn.send(chatHistory.value)
           }
         })
       }
@@ -161,6 +177,10 @@ const sendMessage = () => {
 const receiveMessage = (data) => {
   chatHistory.value.push({ ...data, id: userId.value, date: Date.now() })
 }
+
+onUnmounted(() => {
+  if (peer) peer.destroy()
+})
 </script>
 
 <style scoped>
